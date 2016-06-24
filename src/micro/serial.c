27cac9cc6cb1bb2ce4e0bbe73fc3dc1aa3745876
josndrew <josndrew@users.noreply.github.com>
2016-06-24 18:33:10 -0400

    Fixed USART Serial Communication.

--- src/micro/serial.c
+++ src/micro/serial.c
@@ -12,13 +12,7 @@
 #define BAUDRATE 9600
 #define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)
 
-void USART_init(void){
 
-	UBRR0H = (uint8_t)(BAUD_PRESCALLER>>8);
-	UBRR0L = (uint8_t)(BAUD_PRESCALLER);
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-	UCSR0C = (3<<UCSZ00);
-}
 
 unsigned char USART_receive(void){
 
--- src/micro/test.c
+++ src/micro/test.c
@@ -6,33 +6,48 @@
  */
 #undef F_CPU
 #define F_CPU 16000000UL
+#define BAUDRATE 9600
+#define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
 #include <micro/serial.h>
 
+int is_running_flag = 1;
 
 void ioinit(void);      //Initializes IO
 
+void USART_init(void){
+
+	UBRR0H = (uint8_t)(BAUD_PRESCALLER>>8);
+	UBRR0L = (uint8_t)(BAUD_PRESCALLER);
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	UCSR0C = (3<<UCSZ00);
+}
 
 int main(void){
-	USART_init();        //Call the USART initialization code
+
 	ioinit(); //Setup IO pins and defaults
+	USART_init();        //Call the USART initialization code
+
 
 	char String[256];
 	int count = 0;
+	is_running_flag = 1;
 
-	while(1)
+	while(is_running_flag)
 	{
+
 		PORTB = 0x01;
 		_delay_ms(100);
 
 		if (count > 100){
 			count = 0;
+			is_running_flag = 0;
 		}
 
-		sprintf(String, "%d\n", count, count%3);
+		sprintf(String, "Hello\n", count);
 		count++;
 
 		USART_putstring(String);
@@ -43,8 +58,6 @@ int main(void){
 
 	return 0;
 }
-
-
 
 void ioinit (void)
 {
